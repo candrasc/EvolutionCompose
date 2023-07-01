687ca2fad7f2a2ae75acc3baa803cd5473387df8
Charles Andrascik <65090096+candrasc@users.noreply.github.com>
2023-07-01 13:38:14 +0200

    Working examples

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,12 @@
+import org.jetbrains.compose.compose
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("multiplatform")
-    id("org.jetbrains.compose")
+    kotlin("jvm") version "1.5.31"
+    id("org.jetbrains.compose") version "1.0.0"
 }
+
 
 group = "com.example"
 version = "1.0-SNAPSHOT"
@@ -14,20 +17,21 @@ repositories {
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
 }
 
-kotlin {
-    jvm {
-        jvmToolchain(11)
-        withJava()
-    }
-    sourceSets {
-        val jvmMain by getting {
-            dependencies {
-                implementation(compose.desktop.currentOs)
-            }
-        }
-        val jvmTest by getting
-    }
+dependencies {
+    implementation(compose.desktop.currentOs)
 }
+
+//dependencies {
+//    implementation(compose.desktop.currentOs)
+//    implementation("androidx.core:core-ktx:1.10.1")
+//    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.2")
+//    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.2")
+//}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "11"
+}
+
 
 compose.desktop {
     application {
